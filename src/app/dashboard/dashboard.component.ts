470d0cc0b5da0def3febf69762ee5ee9b139523f
LuisMejiasR <luisme.1993@gmail.com>
2025-02-20 22:05:04 -0300

    feat: adding dashboard components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  Designation: string = "Team Leader";
+  Username: string = "Luis Mejías";
+  NoOfTeamMembers: number = 67;
+  TotalCostOfAllProjects: number = 240;
+  PendingTasks: number = 15;
+  UpComingProjects: number = 2;
+  ProjectCost: number = 2113507;
+  CurrentExpenditure: number = 96788;
+  AvailableFunds: number = 52536;
 
+  ngOnInit(): void {
+  }
 }
